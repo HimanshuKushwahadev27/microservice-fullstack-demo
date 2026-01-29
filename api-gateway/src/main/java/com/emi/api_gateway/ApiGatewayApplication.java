52f3a084e22a41c4d5d27c5d62864e7dbfd9be86
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-01-29 13:44:01 +0530

    TL-27,TL-19: Api gateway(webflux) , connected Microservices

--- api-gateway/src/main/java/com/emi/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/emi/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.emi;
+package com.emi.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-gateway/src/test/java/com/emi/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/emi/api_gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.emi;
+package com.emi.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
