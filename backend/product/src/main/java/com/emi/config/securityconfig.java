4ac5c0a45217e8ef271497bfa28af83545a9be42
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-02-20 02:52:30 +0530

    TL-45 cingured the grfana stack

--- backend/product/src/main/java/com/emi/config/securityconfig.java
+++ backend/product/src/main/java/com/emi/config/securityconfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+
 @Configuration
 public class securityconfig {
 
@@ -16,5 +19,10 @@ public class securityconfig {
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     return http.build();
+	}
+	
+	@Bean
+	ObservedAspect observedAspect(ObservationRegistry registry) {
+		return new ObservedAspect(registry);
 	}
 }
