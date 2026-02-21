bd35b846b3c6f42b8991818de6905a8d7b01393b
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-02-22 03:10:57 +0530

    TL-47 manifests, and finalized the configs

--- backend/Order/src/main/java/com/emi/Order/client/InventoryClient.java
+++ backend/Order/src/main/java/com/emi/Order/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "inventory",
-             url = "http://inventory:8080"
+             url = "${services.inventory.url}"
               )
 public interface InventoryClient {
 
